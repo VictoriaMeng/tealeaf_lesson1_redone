8b2c143fd56a1d269cf69bd7d792dcbbef7322ec
Victoria Meng <vym205@nyu.edu>
2015-12-17 20:26:50 -0500

    changed method names for clarity

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -6,7 +6,7 @@ def say(message)
   puts "=> #{message}"
 end
 
-def player_is_rock(opponent_hand)
+def result_player_rock(opponent_hand)
   if opponent_hand == "rock"
     "You picked Rock - Your opponent picked Rock. - It's a tie!"
   elsif opponent_hand == "paper"
@@ -16,7 +16,7 @@ def player_is_rock(opponent_hand)
   end
 end
 
-def player_is_paper(opponent_hand)
+def result_player_paper(opponent_hand)
   if opponent_hand == "rock"
     "You picked Paper - Your opponent picked Rock. - You win!"
   elsif opponent_hand == "paper"
@@ -26,7 +26,7 @@ def player_is_paper(opponent_hand)
   end
 end
 
-def player_is_scissors(opponent_hand)
+def result_player_scissors(opponent_hand)
   if opponent_hand == "rock"
     "You picked Scissors - Your opponent picked Rock. - You lose!"
   elsif opponent_hand == "paper"
@@ -44,18 +44,18 @@ loop do
   opponent_hand = HANDS.sample
 
   if input.downcase == "r" || input.downcase == "rock"
-    say player_is_rock(opponent_hand)
+    say result_player_rock(opponent_hand)
   elsif input.downcase == "p" || input.downcase == "paper"
-    say player_is_paper(opponent_hand)
+    say result_player_paper(opponent_hand)
   elsif input.downcase == "s" || input.downcase == "scissors"
-    say player_is_scissors(opponent_hand)
+    say result_player_scissors(opponent_hand)
   else
     say "You can only pick Rock, Paper, or Scissors! Choose one: (R/P/S)"
     redo
   end
 
   say "Press 'Y' to play again. Press any other key to exit."
-  break unless gets.chomp.downcase == "y" 
+  break if gets.chomp.downcase != "y" 
 end
 
 say "Thanks for playing!"
